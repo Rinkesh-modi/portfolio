fc3e66aee1e206dbf062ecda9ef67434e15f8928
Rinkesh-modi <rinkeshmodi012@gmail.com>
2024-09-15 22:28:01 +0530

    About Section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,10 +6,10 @@ export const Card = ({
   children,
 }: PropsWithChildren<{ className?: string }>) => {
   return (
-    <div>
+    <>
       <div
         className={twMerge(
-          "bg-gray-800 rounded-3xl relative after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 z-0 after:z-10 after:rounded-3xl after:outline-white/20 overflow-hidden after:pointer-events-none p-6",
+          "bg-gray-800 rounded-3xl relative after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 z-0 after:z-10 after:rounded-3xl after:outline-white/20 overflow-hidden after:pointer-events-none ",
           className
         )}
       >
@@ -19,6 +19,6 @@ export const Card = ({
         ></div>
         {children}
       </div>
-    </div>
+    </>
   );
 };
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -56,7 +56,7 @@ export const TestimonialsSection = () => {
             {testimonials.map((testimonial) => (
               <Card
                 key={testimonial.name}
-                className="max-w-xs md:max-w-md md:p-8 "
+                className="max-w-xs md:max-w-md p-6 md:p-8 "
               >
                 <div className="flex gap-4 items-center">
                   <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
@@ -73,7 +73,7 @@ export const TestimonialsSection = () => {
                     </div>
                   </div>
                 </div>
-                <p className="mt-4 md:mb-6 md:mt-6 text-sm md:text-base">
+                <p className="mt-4  md:mt-6 text-sm md:text-base">
                   {testimonial.text}
                 </p>
               </Card>
